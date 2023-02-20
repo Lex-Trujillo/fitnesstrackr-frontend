ca380cee16efbde36adc2eb55d8fd87cdacbf6c4
Lex Trujillo <flyingskunkmonkey@gmail.com>
2023-02-19 19:59:45 -0500

    fun

--- src/components/CreateActivity.js
+++ src/components/CreateActivity.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import { createActivity } from './api';
+import { createActivity } from '../api';
 
 const CreateActivity = () => {
 const [name, setName] = useState('')
